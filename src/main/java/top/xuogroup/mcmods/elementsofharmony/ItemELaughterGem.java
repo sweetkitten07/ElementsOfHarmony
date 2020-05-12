0fbf36f815fd72217afe2b5171a0f9b7c9e7f343
Twilight Sparkle <twilight_princess_sparkle@outlook.com>
2020-05-12 19:14:10 +0800

    Add New Item: Laughter Gem

---
+++ src/main/java/top/xuogroup/mcmods/elementsofharmony/ItemELaughterGem.java
@@ -0,0 +1,9 @@
+package top.xuogroup.mcmods.elementsofharmony;
+
+import net.minecraft.item.Item;
+
+public class ItemELaughterGem extends Item{
+    public ItemELaughterGem(){
+        super(new Item.Properties().group(ModGroup.elementsofharmonyGroup));
+    }
+}
--- src/main/java/top/xuogroup/mcmods/elementsofharmony/ItemRegistry.java
+++ src/main/java/top/xuogroup/mcmods/elementsofharmony/ItemRegistry.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, "elementsofharmony");
-    public static RegistryObject<Item> itemEMagicGem = ITEMS.register("item_e_magic_gem", ItemEMagicGem::new);
     public static RegistryObject<Item> itemELoyaltyGem = ITEMS.register("item_e_loyalty_gem", ItemELoyaltyGem::new);
+    public static RegistryObject<Item> itemELaughterGem = ITEMS.register("item_e_laughter_gem", ItemELaughterGem::new);
+    public static RegistryObject<Item> itemEMagicGem = ITEMS.register("item_e_magic_gem", ItemEMagicGem::new);
 }
