3a1eed9b38a69023789b29ee854aaa0a24ba8377
Twilight Sparkle <twilight_princess_sparkle@outlook.com>
2020-05-13 13:32:08 +0800

    Remove Unnecessary Import

--- src/main/java/top/xuogroup/mcmods/elementsofharmony/ElementsOfHarmony.java
+++ src/main/java/top/xuogroup/mcmods/elementsofharmony/ElementsOfHarmony.java
@@ -1,7 +1,6 @@
 package top.xuogroup.mcmods.elementsofharmony;
 
 
-import net.minecraft.client.util.SearchTreeManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
